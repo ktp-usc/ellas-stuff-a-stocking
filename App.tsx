07d3fa1cb7f5864510f4ca33c62d1cbe9ab03ac8
Brett-Eck <bertteckstrom@gmail.com>
2025-11-03 23:45:06 +0000

    Navigation bar is now fully fuctional and lets you access all pages

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Navigation } from "./components/Navigation";
 import { Home } from "./components/pages/Home";
-import { About } from "./components/pages/About";
-import { GetInvolved } from "./components/pages/GetInvolved";
+import { About } from "./app/about/page";
+import { GetInvolved } from "./app/get-involved/page";
 
 export default function App() {
   return (
