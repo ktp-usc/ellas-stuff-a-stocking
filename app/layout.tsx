455e1cc32857cf5c649d03bdc18ae788036904aa
Brett-Eck <bertteckstrom@gmail.com>
2025-11-03 21:52:07 +0000

    added navigation bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import {Navigation} from "../components/Navigation";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,8 +26,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
+        
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navigation />
         {children}
       </body>
     </html>
