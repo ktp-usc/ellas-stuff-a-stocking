2dfc5302c1642eee820e25235cb367c388505ad8
ellemehltretter <elle.mehltretter@gmail.com>
2025-11-09 18:17:32 -0500

    Added logo to navigation bar (#5)
    * Added logo to navigation bar
    
    * Added a footer with the logo
    
    ---------
    
    Co-authored-by: mehlt <mehltretterelle@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import {Navigation} from "../components/Navigation";
+import {Footer} from "../components/Footer";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <Navigation />
         {children}
+        <Footer />
       </body>
     </html>
   );
