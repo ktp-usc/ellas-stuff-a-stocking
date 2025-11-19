7858d62b318ad207aeb6fca4e932178270d47a97
Braden Guliano <72895782+bguliano@users.noreply.github.com>
2025-11-19 00:50:24 -0500

    ELF-24 attempt fix within next.config

--- next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  output: "export",
-  images: { unoptimized: true },
-  basePath: process.env.NODE_ENV === "production" ? "/ellas-stuff-a-stocking" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/ellas-stuff-a-stocking/" : ""
-}
